fb76bb006e5604bb05ae5dec3e212b72a5d2da36
SAD-FROG-PEPE <92579146+SAD-FROG-PEPE@users.noreply.github.com>
2022-05-28 13:48:05 +0300

    7

---
+++ Practice7/MireaProject/app/src/main/java/ru/mirea/ivanova/mireaproject/Info.java
@@ -0,0 +1,49 @@
+package ru.mirea.ivanova.mireaproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Info extends Fragment {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private TextView timeField;
+    private String host = "time-b.nist.gov";
+    private int port = 13;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_info, container, false);
+        timeField = view.findViewById(R.id.textTimeView);
+        view.findViewById(R.id.showTime).setOnClickListener(this::updateInfo);
+        return view;
+    }
+
+    private void updateInfo(View view){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String time = "";
+        try {
+            Socket socket = new Socket(host, port);
+            BufferedReader reader = SocketUtils.getReader(socket);
+            reader.readLine();
+            time = reader.readLine();
+            Log.d(TAG, time);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        timeField.setText(time);
+    }
+}
